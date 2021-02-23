66921c18b7207eab969688f06bae3c8cfbc5cb05
Arjun T H <arjun.thekoot@research.iiit.ac.in>
2021-02-23 12:02:31 +0530

    Base code

---
+++ Assignment2/Final/readme.txt
@@ -0,0 +1,1 @@
+When will that be? xD
---
+++ Assignment2/Trials/23_02/ga.py
@@ -0,0 +1,89 @@
+import numpy as np
+import client as ta
+import json
+# Add our Secret here...
+SECRET = ""
+#!!! Go through the code once and do a trial run xD... I haven't run it..
+# TODO: We need to fig out these values... hmm
+MUTATION_PERC = 0.8
+MUTATION_RANGE = 1
+POPULATION_SIZE = 20
+MATE_POOL_SIZE = 10
+MAX_GEN = 20
+initial_chromosome = []
+
+
+def mutate_children(children, low=-MUTATION_RANGE, high=MUTATION_RANGE):
+    for i in range(len(children)):
+        noise = np.random.uniform(low=low, high=high, size=children[i].shape)
+        indices = np.random.choice(np.arange(
+            children[i].size), replace=False, size=int(children[i].size*(1-MUTATION_PERC)))
+        noise[indices] = 0
+        children[i] += noise
+    return np.clip(children, -10, 10)
+
+
+def get_fitness(chromosomes):
+    fitness = []
+    for chromosome in chromosomes:
+        ta_answer = ta.get_errors(SECRET, list(chromosome))
+        fitness.append(ta_answer[0]+ta_answer[1])
+    return np.array(fitness)
+
+
+def cross(parent1, parent2):
+    point = np.random.randint(4, 7)
+    child1 = np.concatenate((parent1[:point], parent2[point:]), axis=0)
+    child2 = np.concatenate((parent2[:point], parent1[point:]), axis=0)
+    return child1, child2
+
+
+def breed(selected_population):
+    children = []
+    child1, child2 = cross(selected_population[0], selected_population[1])
+    children.append(child1)
+    children.append(child2)
+    while(True):
+
+        child1, child2 = cross(
+            np.choice(selected_population), np.choice(selected_population))
+        if child1 not in children:
+            children.append(child1)
+        if len(children) == POPULATION_SIZE-MATE_POOL_SIZE:
+            break
+        if child2 not in children:
+            children.append(child2)
+        if len(children) == POPULATION_SIZE-MATE_POOL_SIZE:
+            break
+
+    return mutate_children(np.array(children))
+
+
+def get_init(chromosome):
+    '''Gets the initial Chromosomes'''
+    temp = [list(chromosome) for i in range(POPULATION_SIZE)]
+    temp = np.array(temp, dtype=np.double)
+    temp = mutate_children(temp, -1, 1)
+    temp[0] = chromosome
+    return temp
+
+
+# TODO: Should this be read from file? That is, will the file overfit.txt be in evaluations or should it be hard-coded? ASK A TA...
+with open("overfit.txt", "r") as f:
+    initial_chromosome = json.load(f)
+
+population = get_init(initial_chromosome)
+fitness = []
+
+for gen in range(MAX_GEN+1):
+    fitness = get_fitness(population)
+    sorted_fitness_index = fitness.argsort()
+    population = population[sorted_fitness_index]
+    fitness = fitness[sorted_fitness_index]
+    selected_population = population[: MATE_POOL_SIZE]
+    selected_fitness = fitness[: MATE_POOL_SIZE]
+    children = breed(selected_population, selected_fitness)
+    population = np.concatenate((selected_population, children), axis=0)
+
+final_fitness = np.min(get_fitness(population))
+print("Answer", final_fitness)
