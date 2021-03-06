2b7f4471b6f69786dde0e419b8c3d717a2d3c67b
tgv2002 <gokul.vamsi@research.iiit.ac.in>
2021-03-06 22:36:01 +0530

    Final run done

--- Assignment2/Trials/6_03/ga.py
+++ Assignment2/Trials/6_03/ga.py
@@ -178,7 +178,7 @@ parents = parents[POPULATION_SIZE-MATE_POOL_SIZE:]
 parent_fitness = parent_fitness[POPULATION_SIZE-MATE_POOL_SIZE:]
 print("---------")
 print()
-currgen = 20
+currgen = 30
 for gen in range(1, MAX_GEN+1):
     print()
     print(">>>>>>>>>")
