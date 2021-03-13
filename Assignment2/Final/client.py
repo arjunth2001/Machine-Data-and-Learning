2b2a9e31d9ec4c3cdcdc7846359eef97566836a8
Arjun T H <arjun.thekoot@research.iiit.ac.in>
2021-03-13 20:29:45 +0530

    First run setup

--- Assignment2/Final/client.py
+++ Assignment2/Final/client.py
@@ -51,9 +51,7 @@ if __name__ == "__main__":
     # topper = [-3.43443138e-02, - 2.38063335e-12, - 2.69250840e-13, - 4.68896595e-04,
     # - 3.90426623e-03, - 1.97864585e-15, - 2.77056954e-05,  2.35379033e-05,
     # - 1.95462508e-06, - 1.55062288e-08,  9.32455255e-10]
-    topper = [1.44492243e-02, - 3.63335347e-02, - 1.35492094e-01, - 5.75856595e-01,
-              - 2.89682141e-10, - 1.73843685e-15, - 4.87113653e-02,  2.58025239e-05,
-              - 1.89688247e-06, - 1.62582575e-08, 8.92756185e-10]
+    topper = [0]*11
     # print(get_errors('z60uCu1jsJeEi4n96iH7qwpMMnvIO1BEdnbC38CokXIn9y9lSR', topper))
   # print(get_overfit_vector('SECRET_KEY'))
     print(submit('z60uCu1jsJeEi4n96iH7qwpMMnvIO1BEdnbC38CokXIn9y9lSR', topper))
