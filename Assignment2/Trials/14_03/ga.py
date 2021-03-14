a7c66d3698feaa2f5229789fca21a3f506152891
tgv2002 <gokul.vamsi@research.iiit.ac.in>
2021-03-14 10:25:55 +0530

    Second minirun done

--- Assignment2/Trials/14_03/ga.py
+++ Assignment2/Trials/14_03/ga.py
@@ -10,7 +10,7 @@ SELECT_TOP_KIDS = 3
 MATE_POOL_SIZE = 15
 MAX_GEN = 6
 FACTOR = 1
-SUM_FACTOR = 3
+SUM_FACTOR = 1
 initial_chromosome = []
 minVal = None
 minguy = None
@@ -200,7 +200,7 @@ parents = parents[POPULATION_SIZE-MATE_POOL_SIZE:]
 parent_fitness = parent_fitness[POPULATION_SIZE-MATE_POOL_SIZE:]
 print("---------")
 print()
-currgen = 0
+currgen = 6
 for gen in range(1, MAX_GEN+1):
     print()
     print(">>>>>>>>>")
