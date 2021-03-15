ee3fd78e0b82b5900a07e68341399fd211aa3b1c
Arjun T H <arjun.thekoot@research.iiit.ac.in>
2021-03-15 20:30:14 +0530

    Me done. This project sed

--- Assignment2/Final/client.py
+++ Assignment2/Final/client.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     # topper = [-3.43443138e-02, - 2.38063335e-12, - 2.69250840e-13, - 4.68896595e-04,
     # - 3.90426623e-03, - 1.97864585e-15, - 2.77056954e-05,  2.35379033e-05,
     # - 1.95462508e-06, - 1.55062288e-08,  9.32455255e-10]
-    topper = [-0.01654691, 0.,        0., - 0.21185164, - 0.52008097,  0.,
-              - 0.06707354, 0.,       0., 0.,     0.]
-    # print(get_errors('z60uCu1jsJeEi4n96iH7qwpMMnvIO1BEdnbC38CokXIn9y9lSR', topper))
-  # print(get_overfit_vector('SECRET_KEY'))
+    topper = [-0.01549962, 0.0, 0.0, -0.21181048, -
+              0.52008022, 0.0, -0.06102663, 0.0, 0.0, 0.0, 0.0]
+    print(get_errors('z60uCu1jsJeEi4n96iH7qwpMMnvIO1BEdnbC38CokXIn9y9lSR', topper))
+    # print(get_overfit_vector('SECRET_KEY'))
     print(submit('z60uCu1jsJeEi4n96iH7qwpMMnvIO1BEdnbC38CokXIn9y9lSR', topper))
