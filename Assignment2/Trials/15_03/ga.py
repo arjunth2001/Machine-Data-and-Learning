015eb68de1ebf3ce53ced316ad77078b8c4a3b39
Arjun T H <arjun.thekoot@research.iiit.ac.in>
2021-03-15 10:25:03 +0530

    Works

--- Assignment2/Trials/15_03/ga.py
+++ Assignment2/Trials/15_03/ga.py
@@ -8,7 +8,7 @@ POPULATION_SIZE = 16
 SELECT_TOP_PARENTS = 1
 SELECT_TOP_KIDS = 3
 MATE_POOL_SIZE = 8
-MAX_GEN = 12
+MAX_GEN = 24
 FACTOR = 1
 SUM_FACTOR = 1
 initial_chromosome = []
@@ -38,10 +38,10 @@ def get_fitness(chromosomes):
     print()
     print("XXXXX-----XXXXX")
     for chromosome in chromosomes:
-        #ta_answer = ta.get_errors(SECRET, list(chromosome))
+        ta_answer = ta.get_errors(SECRET, list(chromosome))
         requests += 1
-        ta_answer = [np.random.uniform(
-            10000, 1000000), np.random.uniform(10000, 100000)]
+        # ta_answer = [np.random.uniform(
+        # 10000, 1000000), np.random.uniform(10000, 100000)]
         if minVal == None:
             minVal = ta_answer
             minguy = chromosome
